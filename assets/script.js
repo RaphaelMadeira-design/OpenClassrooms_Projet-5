2bf8579e99b52838589a7a4873688f798a15274f
Raphaël Madeira <raphael.m21@gmail.com>
2024-10-31 19:17:50 +0100

    update script.js

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,43 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Sélection de la bannière
+const bannerImg = document.querySelector('.banner-img');
+// Sélection des flèches
+const left = document.querySelector('.arrow_left');
+const right = document.querySelector('.arrow_right');
+// Sélection des points
+const dots = document.querySelectorAll('.dot');
+// Position de la première diapo
+let firstSlide = 0;
+
+// Événement du clic sur la flèche gauche
+left.addEventListener('click', function () {
+    firstSlide = (firstSlide - 1);
+    updateSlider(firstSlide, 'left');
+    updateDots(firstSlide); // Mettez à jour les points indicateurs
+});
+
+// Événement du clic sur la flèche droite
+right.addEventListener('click', function () {
+    firstSlide = (firstSlide + 1) ;
+    updateSlider(firstSlide, 'right');
+    updateDots(firstSlide); // Mettez à jour les points indicateurs
+});
+
+// Changer l'image et le texte
+function updateSlider(index, direction) {
+      
+    // Mettre à jour l'image
+    const imagePath = `assets/images/slideshow/${slides[firstSlide].image}`;
+    bannerImg.src = imagePath;
+
+    // Mettre à jour le texte
+    const tagLine = slides[firstSlide].tagLine;
+    document.querySelector('p').innerHTML = tagLine;
+}
+
+
+// Afficher la première diapositive au chargement de la page
+updateSlider(firstSlide);
